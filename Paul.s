a332933b36f86c366d7b90fd20781024068a904b
pauls21033 <107385310+pauls21033@users.noreply.github.com>
2022-08-01 19:01:54 -0500

    Add files via upload

---
+++ Paul.s
@@ -0,0 +1,21 @@
+name_gv = ["age", "workclass",
+           "fnlwgt",
+           "education",
+           "education-num",
+           "marital-status",
+           "occupation",
+           "relationship",
+           "race",
+           "sex",
+           "capital-gain",
+           "capital-loss",
+           "hours-per-week",
+           "native-country",
+           "annual-income"
+          ]
+
+df.info()
+
+
+data.columns = ["age","workclass","fnlwgt","education","education-num","marital-status","occupation","relationship",
+                "race","sex","capital-gain","capital-loss","hours-per-week","native-country",">50K, <=50K"]
